eaf272f0516a99270f8ef17abdbdcbab2b489669
Steve <steve@Macs-MBP-5.home>
2016-11-25 13:24:35 -0500

    Refactoring

--- app.js
+++ app.js
@@ -2,25 +2,8 @@
 
 require('dotenv').config();
 var WebServer = require("./lib/webserver");
-var MatrixService = require("./lib/matrix-service");
-
-var LedMatrix;
-
-// mock the led matrix during development
-if (process.env.DEVELOPMENT) {
-    LedMatrix = require("./lib/matrix-mock");
-}
-else {
-    LedMatrix = require("node-rpi-rgb-led-matrix");
-}
-
-
-// turn on the matrix!
-var matrixService = new MatrixService(new LedMatrix(32));
-matrixService.fill(255, 50, 100);
-//matrixService.setFrame();
 
 // start the webserver!
-var webserver = new WebServer(process.env.CLIENT || './client', matrixService);
+var webserver = new WebServer(process.env.CLIENT || './client');
 webserver.start();
 
